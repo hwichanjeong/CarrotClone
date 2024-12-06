4190dca6f01579bf12f4d1f8d0d05739caadcc95
hwichanjeong <89827845+hwichanjeong@users.noreply.github.com>
2024-12-06 16:11:40 +0900

    자동 로그인 로직 추가

--- app/src/main/java/com/example/carrotmarketclone/LoginActivity.kt
+++ app/src/main/java/com/example/carrotmarketclone/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.carrotmarketclone
 
 import DB.DBHelper
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -14,6 +15,7 @@ import com.example.carrotmarketclone.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding : ActivityLoginBinding
     var DB: DBHelper?=null
+    lateinit var sharedPreferences : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +29,14 @@ class LoginActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         DB = DBHelper(this)
+        sharedPreferences = getSharedPreferences("UserInfo", MODE_PRIVATE)
+        val edit = sharedPreferences.edit()
+        // 자동로그인 체크 여부 확인
+        if (binding.autoLogin.isChecked) {
+
+            edit.putBoolean("isChecked", true)
+            edit.apply()
+        }
 
         val loginBtn = binding.loginBtn
         val editId = binding.editTextId
@@ -42,6 +52,10 @@ class LoginActivity : AppCompatActivity() {
                 val checkUserpass = DB!!.checkUserpass(user,pwd)
                 if (checkUserpass == true){
                     Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+                    edit.putString("userId", user)
+                    edit.putString("userPwd", pwd)
+
+                    edit.apply()
 
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
@@ -54,6 +68,27 @@ class LoginActivity : AppCompatActivity() {
         binding.signUp.setOnClickListener{
             val loginIntent = Intent(this, SignUpActivity::class.java)
             startActivity(loginIntent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val user = sharedPreferences.getString("userId", "")
+        val pwd = sharedPreferences.getString("userPwd", "")
+        if (sharedPreferences.getBoolean("isChecked",true)){
+            if (user == "" || pwd == ""){
+                return
+            } else {
+                val checkUserpass = DB!!.checkUserpass(user,pwd)
+                if (checkUserpass == true){
+                    Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "아이디와 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
