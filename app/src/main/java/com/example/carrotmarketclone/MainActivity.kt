5b61faf29732809976b42d99d623c197d3b6f88d
hwichanjeong <89827845+hwichanjeong@users.noreply.github.com>
2024-12-06 14:58:39 +0900

    메인화면 구성

--- app/src/main/java/com/example/carrotmarketclone/MainActivity.kt
+++ app/src/main/java/com/example/carrotmarketclone/MainActivity.kt
@@ -9,6 +9,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import com.example.carrotmarketclone.Fragment.ChatFragment
+import com.example.carrotmarketclone.Fragment.HomeFragment
+import com.example.carrotmarketclone.Fragment.LifeFragment
+import com.example.carrotmarketclone.Fragment.MapFragment
+import com.example.carrotmarketclone.Fragment.MyFragment
 import com.example.carrotmarketclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +31,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setBottomNavigationView()
+
+        // 앱 초기 실행 시 홈화면으로 설정
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_home
+        }
 
         initSpinner()
     }
@@ -57,5 +69,33 @@ class MainActivity : AppCompatActivity() {
                 }
 
             }
+    }
+
+    fun setBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, HomeFragment()).commit()
+                    true
+                }
+                R.id.fragment_life -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, LifeFragment()).commit()
+                    true
+                }
+                R.id.fragment_map -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, MapFragment()).commit()
+                    true
+                }
+                R.id.fragment_chat -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, ChatFragment()).commit()
+                    true
+                }
+                R.id.fragment_my -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, MyFragment()).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
